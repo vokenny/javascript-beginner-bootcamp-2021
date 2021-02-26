a7da03f27b8d91e05b4850a75f730c772a4b1c2e
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-02-26 13:31:53 +0000

    Add alternative simpler solution to largest number exerecise

--- section-16-algorithm-challenges/max-number.js
+++ section-16-algorithm-challenges/max-number.js
@@ -1,7 +1,10 @@
 /* Return largest value number in an array */
 
 function largestNum(arr) {
-  return arr.reduce((max, num) => max > num ? max : num)
+  return arr.reduce((max, num) => max > num ? max : num);
+
+  // Alternatively, more simple:
+  // return Math.max(...arr);
 }
 
 var testNums = [10, 4, 101, 0, -1, 18]
